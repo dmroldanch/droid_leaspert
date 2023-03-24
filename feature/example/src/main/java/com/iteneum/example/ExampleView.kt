268030450997441666084835fef9367566691cd3
Daniel Roldan <dmroldanch@gmail.com>
2023-03-24 02:43:07 -0400

    Modularization

---
+++ feature/example/src/main/java/com/iteneum/example/ExampleView.kt
@@ -0,0 +1,12 @@
+package com.iteneum.example
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.iteneum.designsystem.components.LpBasicCard
+
+@Composable
+fun ExampleView(){
+    LpBasicCard() {
+        Text("Hello")
+    }
+}
