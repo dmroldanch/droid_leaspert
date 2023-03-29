71ad39aa0e8ab4e0b6a2843e0ab39dcdb3f64a1b
Irving Gonzalez <gonzalez.irving.1994@outlook.com>
2023-03-29 13:10:00 -0400

    I removed the leasPert folder and deleted the default theme folder, created the theme folder in the design module and added the colors and fonts to the project.

--- app/src/main/java/com/iteneum/leasepert/MainActivity.kt
+++ app/src/main/java/com/iteneum/leasepert/MainActivity.kt
@@ -3,26 +3,15 @@ package com.iteneum.leasepert
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.iteneum.example.ExampleView
-import com.iteneum.leasepert.ui.theme.LeasepertTheme
+import com.iteneum.designsystem.theme.LeasePertTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LeasepertTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ExampleView()
-                }
+            LeasePertTheme {
+
             }
         }
     }
