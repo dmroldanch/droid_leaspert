8235ae7084400f42dfc9be3b837c812ae6309518
Lupillo17 <joseguadaluperivera17@gmail.com>
2023-04-04 13:04:26 -0400

    Added OutlinedTextFieldPassword with example validations (not final validations is just for an example) Addes String resource for not hardcoded text in composable field

---
+++ core/designsystem/src/main/java/com/iteneum/designsystem/components/OutlinedTextField.kt
@@ -0,0 +1,57 @@
+package com.iteneum.designsystem.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import com.iteneum.designsystem.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LpOutlinedTextFieldPassword(
+) {
+    var password by remember { mutableStateOf("") }
+    var showError by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged {
+                if (!it.isFocused) {
+                    showError = password.length < 6 && password.isNotEmpty()
+                }
+            },
+        value = password,
+        onValueChange = {
+            showError = false
+            password = it
+        },
+        label = { Text(text = stringResource(id = R.string.OTFPasswordLabel)) },
+        placeholder = { Text(text = stringResource(id = R.string.OTFPasswordLabel)) },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            if (showError) {
+                Icon(
+                    imageVector = Icons.Filled.Error,
+                    contentDescription = stringResource(id = R.string.OTFPasswordContentDescriptionError),
+                    tint = MaterialTheme.colorScheme.error
+                )
+            }
+        },
+        supportingText = {
+            if (showError) {
+                Text(text = stringResource(id = R.string.OTFPasswordMessageError))
+            }
+        },
+        isError = showError
+    )
+}
