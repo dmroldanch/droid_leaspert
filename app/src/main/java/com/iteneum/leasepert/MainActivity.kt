9ba0b407d4adbc95abacdb495f907f2c306f073e
Andres Ivan Medina Herrera <devandresmedina@gmail.com>
2023-04-04 18:32:33 -0400

    update feature/LP74-OutLinedTextField-Unit

--- app/src/main/java/com/iteneum/leasepert/MainActivity.kt
+++ app/src/main/java/com/iteneum/leasepert/MainActivity.kt
@@ -3,6 +3,13 @@ package com.iteneum.leasepert
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import com.iteneum.designsystem.components.LpOutlinedTextFieldMail
+import com.iteneum.designsystem.components.LpOutlinedTextFieldNumber
+import com.iteneum.designsystem.components.Preview_SingleRadioButton
 import com.iteneum.designsystem.theme.LeasePertTheme
 
 
