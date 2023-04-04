ac391d6a32fe0f86dc1db76dbae225ef32633779
Lupillo17 <joseguadaluperivera17@gmail.com>
2023-04-04 15:28:54 -0400

    Fix border color when component is not focused

--- core/designsystem/src/main/java/com/iteneum/designsystem/components/OutlinedTextField.kt
+++ core/designsystem/src/main/java/com/iteneum/designsystem/components/OutlinedTextField.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -37,7 +38,9 @@ fun LpOutlinedTextFieldPassword(
         colors = TextFieldDefaults.outlinedTextFieldColors(
             focusedBorderColor = MaterialTheme.colorScheme.onPrimary,
             focusedLabelColor = MaterialTheme.colorScheme.onPrimary,
-            cursorColor = MaterialTheme.colorScheme.onPrimary
+            unfocusedBorderColor = MaterialTheme.colorScheme.onPrimary,
+            cursorColor = MaterialTheme.colorScheme.onPrimary,
+            placeholderColor = Color.LightGray
         ),
         label = { Text(text = stringResource(id = R.string.OTFPasswordLabel)) },
         placeholder = { Text(text = stringResource(id = R.string.OTFPasswordLabel)) },
