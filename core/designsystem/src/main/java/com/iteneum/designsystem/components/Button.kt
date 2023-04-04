dc731a5e87c920e3dd312a361dfd59f0091cd001
Usiel Garcia Jimenez <usieloficial@gmail.com>
2023-04-04 15:43:45 -0400

    Add comments and resolved flavor content

--- core/designsystem/src/main/java/com/iteneum/designsystem/components/Button.kt
+++ core/designsystem/src/main/java/com/iteneum/designsystem/components/Button.kt
@@ -1,6 +1,5 @@
 package com.iteneum.designsystem.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -13,18 +12,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+/**
+ * [LpFilledTonalButton] is a button for show in the login UI, is a button for the logic login
+ *
+ * @param modifier Modifier is for specify style and params of the button, like for example the width of the component.
+ * @param textButton TextButton is for set a text what the button show in to the UI
+ * @param onClick High order function for assign functionality to the button.
+ *
+ * @author Usiel Filiberto Garcia Jimenez
+ */
 @Composable
-fun LpFilledTonalButton(modifier: Modifier, textButtonDescription: String, onClick: () -> Unit) {
+fun LpFilledTonalButton(modifier: Modifier, textButton: String, onClick: () -> Unit) {
     FilledTonalButton(
         onClick = onClick,
         modifier.padding(10.dp),
         shape = RoundedCornerShape(12.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.secondary
+            contentColor = MaterialTheme.colorScheme.onPrimary
         )
     ) {
-        Text(textButtonDescription)
+        Text(textButton)
     }
 }
 
