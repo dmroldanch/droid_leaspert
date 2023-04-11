2a54c31e349bcf39c4390805d15c4d7fade1ebf4
juanislas97 <58410224+juanislas77@users.noreply.github.com>
2023-04-11 14:37:09 -0400

    LP 18 profile

--- feature/profile/src/main/java/com/iteneum/profile/presentation/ProfileView.kt
+++ feature/profile/src/main/java/com/iteneum/profile/presentation/ProfileView.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,15 +19,19 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.iteneum.designsystem.components.LpEditFloatingActionButton
-import com.iteneum.designsystem.theme.Drab
+import com.iteneum.designsystem.components.LpOutlinedButton
 import com.iteneum.profile.R
 
 @Composable
-fun ProfileView(){
+fun ProfileView(
+
+) {
     val userPhoto = painterResource(id = R.drawable.gato)
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(all = 7.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 7.dp)
+    ) {
         Row(modifier = Modifier.fillMaxWidth()) {
             Box(modifier = Modifier.padding(horizontal = 0.dp, vertical = 10.dp)) {
                 FilledIconButton(
@@ -34,14 +41,20 @@ fun ProfileView(){
                     Icon(
                         Icons.Filled.ArrowBack,
                         contentDescription = "back button",
-                        tint = Drab
+                        tint = MaterialTheme.colorScheme.onPrimary
                     )
                 }
             }
-            Box(modifier = Modifier
-                .fillMaxWidth(0.75f)
-                .padding(horizontal = 5.dp, vertical = 20.dp)) {
-                Text(text = "Profile", fontSize = 22.sp, color = MaterialTheme.colorScheme.onPrimaryContainer)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.75f)
+                    .padding(horizontal = 5.dp, vertical = 20.dp)
+            ) {
+                Text(
+                    text = "Profile",
+                    fontSize = 22.sp,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
             }
             Column(
                 modifier = Modifier.padding(all = 0.dp),
@@ -67,36 +80,93 @@ fun ProfileView(){
             }
             Column() {
                 Box(modifier = Modifier.padding(vertical = 10.dp)) {
-                    Text(text = "Juan Islas", fontWeight = FontWeight.Medium, fontSize = 18.sp, color = MaterialTheme.colorScheme.onPrimaryContainer)
+                    Text(
+                        text = "Juan Islas",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 18.sp,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
                 }
-                Box(modifier = Modifier.padding(vertical = 5.dp)) {
+                Box(modifier = Modifier.padding(top = 1.dp)) {
                     Text(text = "Apartment #101", color = MaterialTheme.colorScheme.tertiary)
                 }
             }
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 25.dp, end = 25.dp, top = 15.dp, bottom = 7.dp)
+        ) {
             Icon(
-                Icons.Filled.ArrowBack,
-                contentDescription = "back button",
-                tint = Drab
+                Icons.Outlined.Phone,
+                contentDescription = "phone icon",
+                modifier = Modifier.padding(end = 5.dp),
+                tint = MaterialTheme.colorScheme.tertiary
             )
-            Text(text = "(404) 979-2400")
+            Text(
+                text = "(404) 979-2400",
+                fontWeight = FontWeight.Normal,
+                color = MaterialTheme.colorScheme.tertiary
+            )
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp, vertical = 7.dp)
+        ) {
             Icon(
-                Icons.Filled.ArrowBack,
-                contentDescription = "back button",
-                tint = Drab
+                Icons.Outlined.Mail,
+                contentDescription = "mail icon",
+                modifier = Modifier.padding(end = 5.dp),
+                tint = MaterialTheme.colorScheme.tertiary
             )
-            Text(text = "juan.islas@mail.com")
+            Text(
+                text = "juan.islas@mail.com",
+                fontWeight = FontWeight.Normal,
+                color = MaterialTheme.colorScheme.tertiary
+            )
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 25.dp, end = 25.dp, top = 7.dp, bottom = 15.dp)
+        ) {
             Icon(
-                Icons.Filled.ArrowBack,
-                contentDescription = "back button",
-                tint = Drab
+                Icons.Outlined.LocationOn,
+                contentDescription = "location icon",
+                modifier = Modifier.padding(end = 5.dp),
+                tint = MaterialTheme.colorScheme.tertiary
             )
-            Text(text = "4950 Gaidrew, Alpharetta, GA, 30022")
+            Text(
+                text = "4950 Gaidrew, Alpharetta, GA, 30022",
+                fontWeight = FontWeight.Normal,
+                color = MaterialTheme.colorScheme.tertiary
+            )
         }
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp, vertical = 10.dp),
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        LpOutlinedButton(
+            modifier = Modifier.padding(horizontal = 15.dp),
+            textButton = "Payment Methods",
+            //borderColor = MaterialTheme.colorScheme.onPrimary,
+            //containerColor = MaterialTheme.colorScheme.primaryContainer,
+            onClick = {})
+        LpOutlinedButton(
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 0.dp),
+            textButton = "Emergency Contacts",
+            //borderColor = MaterialTheme.colorScheme.onPrimary,
+            //containerColor = MaterialTheme.colorScheme.primaryContainer,
+            onClick = {})
+        LpOutlinedButton(
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 0.dp),
+            textButton = "Log Out",
+            //borderColor = MaterialTheme.colorScheme.onPrimary,
+            //containerColor = MaterialTheme.colorScheme.primaryContainer,
+            onClick = {})
     }
 }
