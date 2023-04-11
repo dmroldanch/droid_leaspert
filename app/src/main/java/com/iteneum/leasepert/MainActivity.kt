51ec8314257afc1c158e851cde7e75895151fc2b
Lupillo17 <joseguadaluperivera17@gmail.com>
2023-04-11 17:27:46 -0400

    Remove function and imports from MainActivity.kt for "ExampleView"

--- app/src/main/java/com/iteneum/leasepert/MainActivity.kt
+++ app/src/main/java/com/iteneum/leasepert/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.iteneum.designsystem.theme.LeasePertTheme
-import com.iteneum.example.ExampleView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LeasePertTheme {
-                ExampleView()
+
             }
         }
     }
