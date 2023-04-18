acb0db3c390ec018eb241696adcbc1ec880d645a
Yaritza Moreno <yaritza.moreno94@outlook.com>
2023-04-18 15:22:57 -0400

    I've created OfficeViewModel and injected it in the View

--- feature/office/src/main/java/com/iteneum/office/presentation/viewmodel/OfficeViewModel.kt
+++ feature/office/src/main/java/com/iteneum/office/presentation/viewmodel/OfficeViewModel.kt
@@ -1,7 +1,5 @@
 package com.iteneum.office.presentation.viewmodel
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
