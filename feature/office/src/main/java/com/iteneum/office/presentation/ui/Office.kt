bf5b254b16e11d2bae2a5e54dbf5179196f1458e
Yaritza Moreno <yaritza.moreno94@outlook.com>
2023-04-18 14:48:57 -0400

    I've added ViewModel

--- feature/office/src/main/java/com/iteneum/office/presentation/ui/Office.kt
+++ feature/office/src/main/java/com/iteneum/office/presentation/ui/Office.kt
@@ -1,4 +1,4 @@
-package com.iteneum.office.presentation
+package com.iteneum.office.presentation.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
--- feature/office/src/main/java/com/iteneum/office/presentation/viewmodel/OfficeViewModel.kt
+++ feature/office/src/main/java/com/iteneum/office/presentation/viewmodel/OfficeViewModel.kt
@@ -1,4 +1,24 @@
 package com.iteneum.office.presentation.viewmodel
 
-class OfficeViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class OfficeViewModel: ViewModel() {
+
+    private val _event = mutableStateOf(true)
+    val event: State<Boolean>
+    get() = _event
+
+    private fun onClick(){
+        viewModelScope.launch(Dispatchers.IO) {
+
+        }
+    }
+
 }
