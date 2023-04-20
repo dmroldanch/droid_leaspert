9390c1247c89c7598be4960af723433a992b733b
Usiel Garcia Jimenez <usieloficial@gmail.com>
2023-04-20 15:24:33 -0400

    Apartment card comments solved 2

--- core/designsystem/src/main/java/com/iteneum/designsystem/components/Card.kt
+++ core/designsystem/src/main/java/com/iteneum/designsystem/components/Card.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
@@ -31,7 +32,9 @@ import com.iteneum.designsystem.R
 import com.iteneum.designsystem.theme.*
 import com.iteneum.designsystem.utils.TextUtils.ONE
 import com.iteneum.designsystem.theme.LeasePertTheme
+import org.w3c.dom.Text
 import java.sql.Timestamp
+import java.util.Date
 
 /**
  * Created [LpGenericCard]
