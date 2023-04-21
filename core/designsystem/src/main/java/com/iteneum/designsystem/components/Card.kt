610ca49f34d7023371de703d4ed22fa57c7f8955
Usiel Garcia Jimenez <usieloficial@gmail.com>
2023-04-20 20:45:53 -0400

    Imports unused removed

--- core/designsystem/src/main/java/com/iteneum/designsystem/components/Card.kt
+++ core/designsystem/src/main/java/com/iteneum/designsystem/components/Card.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 import com.iteneum.designsystem.R
