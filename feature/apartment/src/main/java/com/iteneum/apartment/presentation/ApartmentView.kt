3ff67e425b1a26c01b34b39d8930eb1f4d65b014
Usiel Garcia Jimenez <usieloficial@gmail.com>
2023-04-25 12:03:54 -0400

    Apartment UI for review 1

--- feature/apartment/src/main/java/com/iteneum/apartment/presentation/ApartmentView.kt
+++ feature/apartment/src/main/java/com/iteneum/apartment/presentation/ApartmentView.kt
@@ -3,26 +3,32 @@ package com.iteneum.apartment.presentation
 import androidx.compose.foundation.*
 import com.iteneum.designsystem.theme.LeasePertTheme
 import androidx.compose.foundation.layout.*
-
-
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.iteneum.apartment.R
+import com.iteneum.designsystem.components.LPTitleLarge
 import com.iteneum.designsystem.components.LpGenericChip
 import com.iteneum.designsystem.components.LpOutlinedButton
+import com.iteneum.designsystem.components.LpPaymentsRentCard
+import java.sql.Timestamp
 
+/**
+ * [ApartmentView] The apartment view is the screen where the user watches your payments, repairs, information
+ * [ApartmentContainer] This section is where all the sections are, because all the sections are divided each one
+ * [TitleApartmentScreen] This section is where only to be the tittle of the screen, in this case: "Apartment"
+ * [PaymentsSection] All the params for this sections is for show the current status, date, month and their account,
+ * in this section the user can their account
+ * [RepairsSection] and [RepairStatusRow], work together for show and open a new repairs application with the button,
+ * the user can watch the Open, In Progress and closed repairs status
+ * [InformationSection] is where the user have a their disposable some buttons for leasing documents, apartment photos and current status
+ *
+ * @author Usiel Filiberto Garcia Jimenez
+ */
 @Composable
 @Preview
 fun ApartmentView() {
@@ -31,15 +37,18 @@ fun ApartmentView() {
 
 @Composable
 fun ApartmentContainer() {
-    val dp12 = LeasePertTheme.sizes.midSmallSize
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(start = dp12, end = dp12)
+            .padding(
+                start = LeasePertTheme.sizes.midSmallSize,
+                end = LeasePertTheme.sizes.midSmallSize
+            )
             .verticalScroll(state = rememberScrollState())
     ) {
         Column {
-            PaymentsSection()
+            TitleApartmentScreen()
+            PaymentsSection(Modifier, "Junary", 990.00, Timestamp(23435))
             RepairsSection()
             InformationSection()
         }
@@ -47,43 +56,76 @@ fun ApartmentContainer() {
 }
 
 @Composable
-fun PaymentsSection() {
+fun TitleApartmentScreen() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                bottom = LeasePertTheme.sizes.extraSize18,
+                top = LeasePertTheme.sizes.minorRegularSize
+            )
+    ) {
+        LPTitleLarge(
+            label = stringResource(
+                id = R.string.apartment
+            ),
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+}
+
+@Composable
+fun PaymentsSection(modifier: Modifier, month: String, quantityToPay: Double, date: Timestamp) {
     Column() {
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(bottom = LeasePertTheme.sizes.regularSize),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
             Text(
                 text = stringResource(id = R.string.payments),
                 color = MaterialTheme.colorScheme.tertiary,
-                fontSize = 20.sp
+                fontSize = MaterialTheme.typography.titleSmall.lineHeight
             )
-
             LpGenericChip(label = stringResource(id = R.string.history)) {
-
+                /*TODO with this button could pay their rent payment*/
             }
         }
+        LpPaymentsRentCard(
+            modifier = Modifier.fillMaxWidth(),
+            month = month,
+            quantity = quantityToPay,
+            date = date
+        )
     }
 }
 
 @Composable
 fun RepairsSection() {
     Column() {
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = LeasePertTheme.sizes.regularSize,
+                    bottom = LeasePertTheme.sizes.minorRegularSize
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
             Text(
                 text = stringResource(id = R.string.repairs),
-
                 color = MaterialTheme.colorScheme.tertiary,
-                fontSize = 20.sp
+                fontSize = MaterialTheme.typography.titleSmall.lineHeight
             )
-            
             LpGenericChip(label = "+${stringResource(id = R.string.newA)}") {
-                
+                /*TODO with this button the user could make a repair request for him department*/
             }
         }
-
         RepairStatusRow(
             modifier = Modifier.fillMaxWidth(),
             titleStatusRepairs = stringResource(id = R.string.open)
         )
-
         Divider(
             thickness = LeasePertTheme.sizes.stroke,
             color = MaterialTheme.colorScheme.onPrimary,
@@ -92,12 +134,10 @@ fun RepairsSection() {
                 end = LeasePertTheme.sizes.middleSize
             )
         )
-
         RepairStatusRow(
             modifier = Modifier.fillMaxWidth(),
             titleStatusRepairs = stringResource(id = R.string.in_progress)
         )
-
         Divider(
             thickness = LeasePertTheme.sizes.stroke,
             color = MaterialTheme.colorScheme.onPrimary,
@@ -106,12 +146,10 @@ fun RepairsSection() {
                 end = LeasePertTheme.sizes.middleSize
             )
         )
-
         RepairStatusRow(
             modifier = Modifier.fillMaxWidth(),
             titleStatusRepairs = stringResource(id = R.string.closed)
         )
-
     }
 }
 
@@ -129,14 +167,19 @@ fun RepairStatusRow(
             ), horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            modifier = Modifier.padding(top = LeasePertTheme.sizes.smallSize, bottom = 17.dp),
+            modifier = Modifier.padding(
+                top = LeasePertTheme.sizes.smallSize,
+                bottom = LeasePertTheme.sizes.extraSize18
+            ),
             text = titleStatusRepairs,
             style = MaterialTheme.typography.bodyLarge
         )
-
         Box(
             modifier = Modifier
-                .padding(bottom = LeasePertTheme.sizes.smallerSize, top = 9.dp)
+                .padding(
+                    bottom = LeasePertTheme.sizes.smallerSize,
+                    top = LeasePertTheme.sizes.extraSize10
+                )
                 .size(LeasePertTheme.sizes.largeSize)
                 .clip(MaterialTheme.shapes.extraLarge)
                 .background(color = MaterialTheme.colorScheme.secondaryContainer),
@@ -156,45 +199,25 @@ fun InformationSection() {
         Text(
             text = stringResource(id = R.string.information),
             color = MaterialTheme.colorScheme.tertiary,
-            fontSize = 20.sp
+            fontSize = MaterialTheme.typography.titleSmall.lineHeight
         )
-
         LpOutlinedButton(
-            modifier = Modifier.padding(top = 14.dp),
+            modifier = Modifier.padding(top = LeasePertTheme.sizes.midMediumSize),
             textButton = stringResource(id = R.string.leasing_documents)
         ) {
-
+            /*TODO with this button the user can watch what documents do need for can rent one apartment*/
         }
-
         LpOutlinedButton(
-            modifier = Modifier.padding(top = 7.dp),
+            modifier = Modifier.padding(top = LeasePertTheme.sizes.midMediumSize),
             textButton = stringResource(id = R.string.apartment_photos)
         ) {
-
+            /*TODO with this button the user can watch the photos of the apartments*/
         }
-
         LpOutlinedButton(
-            modifier = Modifier.padding(top = 7.dp),
+            modifier = Modifier.padding(top = LeasePertTheme.sizes.midMediumSize),
             textButton = stringResource(id = R.string.current_status)
         ) {
-
+            /*TODO with this button the user can watch what is the status of the apartment*/
         }
-    }
-}
-
-@Composable
-fun roundFormExperimental(quantityStatusRepairs: Int) {
-    Box(
-        modifier = Modifier
-            .padding(bottom = LeasePertTheme.sizes.smallerSize, top = 9.dp)
-            .size(LeasePertTheme.sizes.largeSize)
-            .clip(MaterialTheme.shapes.extraLarge)
-            .background(color = MaterialTheme.colorScheme.secondaryContainer),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = quantityStatusRepairs.toString(),
-            color = MaterialTheme.colorScheme.onPrimary
-        )
     }
 }
