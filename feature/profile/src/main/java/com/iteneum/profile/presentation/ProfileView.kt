683fe2dd8143bf1002d3223478869f774ab0ab47
juanislas97 <58410224+juanislas77@users.noreply.github.com>
2023-05-03 13:51:43 -0400

    Resolving comments

--- feature/profile/src/main/java/com/iteneum/profile/presentation/ProfileView.kt
+++ feature/profile/src/main/java/com/iteneum/profile/presentation/ProfileView.kt
@@ -72,9 +72,8 @@ fun ProfileView(
                     containerColor = MaterialTheme.colorScheme.inversePrimary,
                     elevation = FloatingActionButtonDefaults.elevation(sizes.nothingSize),
                     icon = Icons.Outlined.Edit,
-                    contentDescription = stringResource(R.string.content_description_edit),
-                    onClicked = {/*TODO expected change in the values of user's phone number, email or address*/ }
-                )
+                    contentDescription = stringResource(R.string.content_description_edit)
+                ) {/*TODO expected change in the values of user's phone number, email or address*/ }
             }
         }
         Row(modifier = Modifier.fillMaxWidth()) {
@@ -119,83 +118,80 @@ fun ProfileView(
                 modifier = Modifier.padding(end = sizes.minorSmallSize),
                 tint = MaterialTheme.colorScheme.tertiary
             )
-        }
-        Text(
-            text = "(404) 979-2400",
-            fontWeight = FontWeight.Normal,
-            color = MaterialTheme.colorScheme.tertiary
-        )
-
-    }
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = sizes.regularSize, vertical = sizes.smallerSize)
-    ) {
-
-        Icon(
-            Icons.Outlined.Mail,
-            contentDescription = stringResource(id = R.string.mail_icon),
-            modifier = Modifier.padding(end = sizes.minorSmallSize),
-            tint = MaterialTheme.colorScheme.tertiary
-        )
-        Text(
-            text = "juan.islas@mail.com",
-            fontWeight = FontWeight.Normal,
-            color = MaterialTheme.colorScheme.tertiary
-        )
-    }
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                start = sizes.regularSize,
-                end = sizes.regularSize,
-                top = sizes.smallerSize,
-                bottom = sizes.smallSize
+            Text(
+                text = "(404) 979-2400",
+                fontWeight = FontWeight.Normal,
+                color = MaterialTheme.colorScheme.tertiary
             )
-    ) {
-        Icon(
-            Icons.Outlined.LocationOn,
-            contentDescription = stringResource(id = R.string.location_icon),
-            modifier = Modifier.padding(end = sizes.minorSmallSize),
-            tint = MaterialTheme.colorScheme.tertiary
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = sizes.regularSize, vertical = sizes.smallerSize)
+        ) {
+            Icon(
+                Icons.Outlined.Mail,
+                contentDescription = stringResource(id = R.string.mail_icon),
+                modifier = Modifier.padding(end = sizes.minorSmallSize),
+                tint = MaterialTheme.colorScheme.tertiary
+            )
+            Text(
+                text = "juan.islas@mail.com",
+                fontWeight = FontWeight.Normal,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = sizes.regularSize,
+                    end = sizes.regularSize,
+                    top = sizes.smallerSize,
+                    bottom = sizes.smallSize
+                )
+        ) {
+            Icon(
+                Icons.Outlined.LocationOn,
+                contentDescription = stringResource(id = R.string.location_icon),
+                modifier = Modifier.padding(end = sizes.minorSmallSize),
+                tint = MaterialTheme.colorScheme.tertiary
+            )
+            Text(
+                text = "4950 Gaidrew, Alpharetta, GA, 30022",
+                fontWeight = FontWeight.Normal,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+        }
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = sizes.minorRegularSize, vertical = sizes.midSmallSize),
+            color = MaterialTheme.colorScheme.onPrimary
         )
-        Text(
-            text = "4950 Gaidrew, Alpharetta, GA, 30022",
-            fontWeight = FontWeight.Normal,
-            color = MaterialTheme.colorScheme.tertiary
-        )
+        LpOutlinedButton(
+            modifier = Modifier
+                .padding(
+                    horizontal = sizes.smallSize,
+                    vertical = sizes.minorSmallSize
+                ),
+            textButton = stringResource(id = R.string.payment_methods),
+            onClicked = { /*TODO high order function for payment methods*/ })
+        LpOutlinedButton(
+            modifier = Modifier
+                .padding(
+                    horizontal = sizes.smallSize,
+                    vertical = sizes.minorSmallSize
+                ),
+            textButton = stringResource(id = R.string.emergency_contacts),
+            onClicked = { /*TODO high order function for emergency contacts*/ })
+        LpOutlinedButton(
+            modifier = Modifier
+                .padding(
+                    horizontal = sizes.smallSize,
+                    vertical = sizes.minorSmallSize
+                ),
+            textButton = stringResource(id = R.string.log_out),
+            onClicked = { /*TODO log out and return to the login view*/ })
     }
-    Divider(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = sizes.minorRegularSize, vertical = sizes.midSmallSize),
-        color = MaterialTheme.colorScheme.onPrimary
-    )
-    LpOutlinedButton(
-        modifier = Modifier
-            .padding(
-                horizontal = sizes.smallSize,
-                vertical = sizes.minorSmallSize
-            ),
-        textButton = stringResource(id = R.string.payment_methods),
-        onClicked = { /*TODO high order function for payment methods*/ })
-    LpOutlinedButton(
-        modifier = Modifier
-            .padding(
-                horizontal = sizes.smallSize,
-                vertical = sizes.minorSmallSize
-            ),
-        textButton = stringResource(id = R.string.emergency_contacts),
-        onClicked = { /*TODO high order function for emergency contacts*/ })
-    LpOutlinedButton(
-        modifier = Modifier
-            .padding(
-                horizontal = sizes.smallSize,
-                vertical = sizes.minorSmallSize
-            ),
-        textButton = stringResource(id = R.string.log_out),
-        onClicked = { /*TODO log out and return to the login view*/ })
 }
-
