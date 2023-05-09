d2b8199bd64ff88a9949853f4af9d6f0d6029557
Irving Gonzalez <gonzalez.irving.1994@outlook.com>
2023-05-09 11:46:12 -0400

    add some updates in some textField components.

--- feature/login/src/main/java/com/iteneum/login/presentation/LoginView.kt
+++ feature/login/src/main/java/com/iteneum/login/presentation/LoginView.kt
@@ -96,7 +96,7 @@ fun LoginView(navigationToDashboard: () -> Unit) {
             onPasswordChanged = { /*TODO("This will change the value of the password")*/ },
             supportTextError = stringResource(R.string.lv_support_text_error),
             isValid = false,
-            value = stringResource(R.string.lv_password)
+            LabelText = stringResource(R.string.lv_password)
         )
         LpFilledTonalButton(
             modifier = Modifier
