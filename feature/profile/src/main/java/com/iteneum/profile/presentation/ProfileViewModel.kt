65e774fb8756da4297d59157b170445beb792cdd
José Miguel <josemiguelgarcia09@gmail.com>
2023-06-26 13:58:21 -0400

    Removal of unnecessary code at the actual project phase

--- feature/profile/src/main/java/com/iteneum/profile/presentation/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/iteneum/profile/presentation/ProfileViewModel.kt
@@ -5,11 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.iteneum.profile.domain.Profile
 import com.iteneum.network.DataState
+import com.iteneum.profile.domain.Profile
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,7 +24,6 @@ class ProfileViewModel @Inject constructor(): ViewModel() {
 
     private val _state: MutableStateFlow<ProfileState> =
         MutableStateFlow(ProfileState.Loading)
-    val state: StateFlow<ProfileState> = _state
 
     var profileModelMutable: Profile by mutableStateOf(Profile())
         private set
@@ -48,24 +46,6 @@ class ProfileViewModel @Inject constructor(): ViewModel() {
         }
     }
 
-    fun getProfileImageFromView(profileImage:String) {
-        profileModelMutable = profileModelMutable.copy(image = profileImage)
-    }
-    fun getProfileNameFromView(profileName:String) {
-        profileModelMutable = profileModelMutable.copy(name = profileName)
-    }
-    fun getProfileApartmentFromView(profileApartment:String) {
-        profileModelMutable = profileModelMutable.copy(apartment = profileApartment)
-    }
-    fun getProfilePhoneNumberFromView(profilePhoneNumber:String) {
-        profileModelMutable = profileModelMutable.copy(phoneNumber = profilePhoneNumber)
-    }
-    fun getProfileEmailFromView(profileEmail:String) {
-        profileModelMutable = profileModelMutable.copy(email = profileEmail)
-    }
-    fun getProfileAddressFromView(profileAddress:String) {
-        profileModelMutable = profileModelMutable.copy(address = profileAddress)
-    }
     fun onClickEditProfileButton() {
         /* TODO - ProfileViewModel onClickEditProfileButton - Logic when user clicks on Profile Edit icon button */
     }
